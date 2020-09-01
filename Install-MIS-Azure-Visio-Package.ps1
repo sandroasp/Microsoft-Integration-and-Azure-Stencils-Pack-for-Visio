9a53d67d7b493fd0d1086c39065b7d76301c8e8a
Sandro Pereira <sandro-pereira@live.com.pt>
2020-09-01 22:51:37 +0100

    Automate install process

---
+++ Install-MIS-Azure-Visio-Package.ps1
@@ -0,0 +1,18 @@
+﻿#########################################################
+#                                                       #
+# Install Microsoft Integration & Azure Stencils Pack   #
+# Author: Sandro Pereira                                #
+#                                                       #
+#########################################################
+
+[String]$location = Split-Path -Parent $PSCommandPath
+[String]$destination = [environment]::getfolderpath('mydocuments') + "\My Shapes"
+
+$files = Get-ChildItem $location -recurse -force -Filter *.vssx
+foreach($file in $files)
+{
+    if($file.PSPath.Contains("Previous Versions") -eq $false)
+    {
+        Copy-Item -Path $file.PSPath -Destination $destination -force
+    }
+}
